e99c86cfa338feeec24a3c116ef3c3593b5d0b45
Jens Balvig <jens@cookpad.com>
2012-10-10 17:08:04 +0900

    get rid of linebreaks

--- spec/lib/pt-flow/ui_spec.rb
+++ spec/lib/pt-flow/ui_spec.rb
@@ -21,11 +21,8 @@ module PT::Flow
       context 'given no args' do
         it "shows lists of tasks - choosing one starts/assigns the task on pt and checks out a new branch." do
           prompt.should_receive(:ask).with("Please select a task to start working on (1-1, 'q' to exit)".bold).and_return('1')
-
           task.should_receive(:update).with(owned_by: 'Jens Balvig').and_return(task)
-
           task.should_receive(:update).with(current_state: 'started').and_return(task)
-
           UI.any_instance.should_receive(:`).with('git checkout -B 1')
 
           ui = UI.new %w{ start }
@@ -34,9 +31,7 @@ module PT::Flow
       context 'given a number' do
         it "starts/assigns the task on pt and checks out a new branch." do
           task.should_receive(:update).with(owned_by: 'Jens Balvig').and_return(task)
-
           task.should_receive(:update).with(current_state: 'started').and_return(task)
-
           UI.any_instance.should_receive(:`).with('git checkout -B 1')
 
           ui = UI.new %w{ start 1 }
@@ -45,5 +40,4 @@ module PT::Flow
     end
 
   end
-
 end
