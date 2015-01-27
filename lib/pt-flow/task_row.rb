3c8db433e80fbac27885f2bcd1b813e899998642
Jens Balvig <jens@balvig.com>
2015-01-27 18:54:06 +0100

    don't format empty strings

--- lib/pt-flow/task_row.rb
+++ lib/pt-flow/task_row.rb
@@ -1,6 +1,7 @@
 class TaskRow < PT::DataRow
   def method_missing(method)
     str = super
+    return unless str.size > 0
     case @record.story_type
     when 'feature' then str.cyan.bold
     when 'chore' then str
